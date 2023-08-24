3c4ebf0b2f23e5eb780fbbeb2b4a1a777b80553c
Monica-Zhang-git <monica.zhang.nz@gmail.com>
2023-08-24 22:06:14 +1200

    Feat: Add Get Post Router

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -18,11 +18,11 @@ router.post("/", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
-    if(post.userId === req.body.userId){
-        await post.deleteOne();
-        res.status(200).json("Post has been deleted.");
-    }else{
-        return res.status(403).json("You can only delete your own post.")
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("Post has been deleted.");
+    } else {
+      return res.status(403).json("You can only delete your own post.");
     }
   } catch (error) {
     return res.status(500).json(error);
@@ -31,17 +31,27 @@ router.delete("/:id", async (req, res) => {
 
 // Update A POST
 router.put("/:id", async (req, res) => {
-    try {
-      const post = await Post.findById(req.params.id);
-      if(post.userId === req.body.userId){
-          await post.updateOne({$set: req.body});
-          res.status(200).json("Post has been updated.");
-      }else{
-          return res.status(403).json("You can only update your own post.")
-      }
-    } catch (error) {
-      return res.status(500).json(error);
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("Post has been updated.");
+    } else {
+      return res.status(403).json("You can only update your own post.");
     }
-  });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+// Get A POST
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
 
 export default router;
