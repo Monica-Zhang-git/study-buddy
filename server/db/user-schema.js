730e6d43ddec781cde9892e12d3b986ffae422fc
Monica-Zhang-git <monica.zhang.nz@gmail.com>
2023-08-24 14:59:47 +1200

    Add User Schema: Capturing Crucial User Information

---
+++ server/db/user-schema.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 20,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+    cover: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    followings: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+
+const User = mongoose.model("User", userSchema);
+export { User };
+
+
+
