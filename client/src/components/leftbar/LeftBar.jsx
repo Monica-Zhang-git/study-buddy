021672e2bf5645533e1b22cbdc72201525fe13c6
Monica-Zhang-git <monica.zhang.nz@gmail.com>
2023-08-30 20:22:08 +1200

    Fix: Fix Left Bar Lack of UserName Bug

--- client/src/components/leftbar/LeftBar.jsx
+++ client/src/components/leftbar/LeftBar.jsx
@@ -15,7 +15,7 @@ function LeftBar(props) {
         <div className="menu">
           <div className="user">
             <PersonOutlinedIcon />
-            <span>{currentUser.name}</span>
+            <span>{currentUser.username}</span>
           </div>
           <div className="item">
             <GroupIcon />
