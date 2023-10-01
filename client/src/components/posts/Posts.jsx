95edc266597a803356d0d1a8c1ba67ae4076e568
Monica-Zhang-git <monica.zhang.nz@gmail.com>
2023-10-01 13:47:59 +1300

    Update: Update Get Post hook

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,34 +1,25 @@
 import "./posts.scss";
 import Post from "../post/Post";
-import { useContext } from "react";
-import { AuthContext } from "../../context/authContext";
 import { makeRequest } from "../../axios";
-import {useQuery} from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
-function Posts({ username }) {
-  const { currentUser } = useContext(AuthContext);
-
+function Posts({ userId }) {
   const {
     isLoading,
     error,
     data: posts,
-  } = useQuery(["Posts", username, currentUser.userId], async () => {
-    const res = username
-      ? // Get Profile Post
-        await makeRequest.get(`/post/${username}`)
-      : // Get Community Post
-        await makeRequest.get(`/post/${currentUser._id}`);
-
+  } = useQuery(["Posts", userId], async () => {
+    const res = await makeRequest.get("/post?userId=" + userId);
     return res.data;
   });
 
-  if (isLoading) return "Loading...";
-
-  if (error) return "An error has occurred: " + error.message;
-
   return (
     <div className="posts">
-      {posts && posts.map((post) => <Post post={post} key={post._id} />)}
+      {error
+        ? "An error has occurred: " + error.message
+        : isLoading
+        ? "Loading...."
+        : posts.map((post) => <Post post={post} key={post._id} />)}
     </div>
   );
 }
